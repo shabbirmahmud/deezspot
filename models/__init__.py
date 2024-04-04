a1115409f209b564c729cac4fd13dfd37ff29a88
farihdzkyy <78370692+farihdzkyy@users.noreply.github.com>
2024-04-04 02:12:48 -1100

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from .smart import Smart
-from .track import Track
-from .album import Album
-from .playlist import Playlist
-from .preferences import Preferences
--- models/playlist.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from .track import Track
-
-class Playlist:
-	def __init__(self) -> None:
-		self.tracks: list[Track] = []
-		self.zip_path = None
