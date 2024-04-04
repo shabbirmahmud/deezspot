aa95e6e8a210550d8814c3a4df6d15b22d721360
farihdzkyy <78370692+farihdzkyy@users.noreply.github.com>
2024-04-04 01:03:50 -1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+README = open("README.md", "r")
+readmed = README.read()
+README.close()
+
+setup(
+	name = "deezloaderclone",
+	version = "2024.04.05",
+	description = "Downloads songs, albums or playlists from deezer and spotify (cloned from https://github.com/An0nimia/deezloader)",
+	long_description = readmed,
+	long_description_content_type = "text/markdown",
+	license = "GNU Affero General Public License v3",
+	python_requires = ">=3.10",
+	author = "farihdzaky",
+	author_email = "farihdzakyy@gmail.com",
+	url = "https://github.com/farihdzkyy/deezloadclone",
+
+	packages = [
+		"deezloader",
+		"deezloader/models", "deezloader/spotloader",
+		"deezloader/deezloader", "deezloader/libutils"
+	],
+
+	install_requires = [
+		"mutagen", "pycryptodome", "requests",
+		"spotipy", "tqdm", "fastapi",
+		"uvicorn[standard]", "librespot"
+	],
+
+	scripts = [
+		"deezloader/bin/deez-dw.py",
+		"deezloader/bin/deez-web.py"
+	]
+)
