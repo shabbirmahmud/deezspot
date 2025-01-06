0c77c4a942c88c450db6c95ea3ed4f3ac1c4ab4b
farihdzkyy <78370692+farihdzkyy@users.noreply.github.com>
2025-01-07 00:20:59 +0700

    Update deezer.py

--- deezloader/examples/deezer.py
+++ deezloader/examples/deezer.py
@@ -8,9 +8,9 @@ from deezloader.deezloader import DeeLogin
 import traceback
 # Login
 dl = DeeLogin(
-            arl='42dfada98821e9ba118c2f72029221533f0cff61fdb2b7bf8cce757b6b34507630817003a0d0f57eb599022f18491d7d0597d9a7d03ebd4725cf8230ad13f77f90c676822e78b7b36a218a1903a810af9f8a04eea6f8eb68288438f63124f69d',
-            email='farihmuhammad75@gmail.com',
-            password='Farih2009@',
+            arl='Your arl',
+            email='email',
+            password='password',
 )
 
 # Download single track with MP3 320kbp
