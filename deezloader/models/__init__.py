5bf7fc965e726db389c077958be9d261bcc1fd3b
farihdzkyy <78370692+farihdzkyy@users.noreply.github.com>
2025-01-07 22:11:14 +0700

    adjusment

--- deezloader/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from deezloader.models.smart import Smart
-from deezloader.models.track import Track
-from deezloader.models.album import Album
-from deezloader.models.playlist import Playlist
-from deezloader.models.preferences import Preferences
-from deezloader.models.episode import Episode
