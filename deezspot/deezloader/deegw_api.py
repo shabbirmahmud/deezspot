b961a950512c88e5602a16a7381eb57cae8360f2
ghostdisco <gith@ghostdisco.me>
2025-07-31 21:45:59 -0400

    must use iterable type as default

--- deezspot/deezloader/deegw_api.py
+++ deezspot/deezloader/deegw_api.py
@@ -107,7 +107,7 @@ class API_GW:
     def get_account_info(cls):
         data = cls.get_user()
         account_info = {
-            'is_premium': 'Free' not in data.get('OFFER_NAME')
+            'is_premium': 'Free' not in data.get('OFFER_NAME', 'UNKNOWN')
         }
         return account_info
 
